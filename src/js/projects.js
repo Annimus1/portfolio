76424b3afb32281ef2096caeb755a39e5b4f190f
pablo <pablo.j98@gmail.com>
2025-01-22 23:09:15 -0400

    add option to swith Spanish and English

--- src/js/projects.js
+++ src/js/projects.js
@@ -1,14 +1,32 @@
 // get containers from html
-const projectsContainer = document.getElementById("projects")
-const certificationContainer = document.getElementById("certification")
-
-
-// variables to save the content
-let projectsContent = ""
-let certificationsContent = ""
-
+const projectsContainer = document.getElementById("projects");
+const certificationContainer = document.getElementById("certification");
+const introductionContainer = document.getElementById("introduction-wrapper");
+const languageBtn = document.getElementById("language-btn");
 
 // representation of content
+const introduction = {"en": {'title':"Hi I'm Pablo Vergara", 'content':[`A venezuelan Software Developer with a solid foundation in cutting-edge
+            technologies like
+            JavaScript, Python, HTML5, Java, SQL, my passion for development shines through in every line of code I
+            write. My primary focus has been on frontend development with tecnologies like React & tailwind Css, where
+            I have honed my ability to create engaging and functional user experiences. However, my thirst for
+            knowledge has led me to delve into the world of backend development, specifically with Java and Spring
+            Boot, where I am excited to explore new possibilities and challenges.`,`My background is not only about applying current technologies but also about
+            quickly
+            adapting to new tools and development environments. I firmly believe in combining creativity with logic to
+            build software products that are not only visually appealing but also efficient and robust in their
+            functionality.`]}, "es":{ 'title':"Hola soy Pablo Vergara", 'content': [`Un Desarrollador de Software venezolano con una sólida base en la vanguardia.
+            tecnologías como
+            JavaScript, Python, HTML5, Java, SQL, mi pasión por el desarrollo se refleja en cada línea de código que
+            escribir. Mi enfoque principal ha sido el desarrollo frontend con tecnologías como React y tailwind Css, donde
+            He perfeccionado mi capacidad para crear experiencias de usuario atractivas y funcionales. Sin embargo, mi sed de
+            conocimientos me han llevado a profundizar en el mundo del desarrollo backend, concretamente con Java y Spring
+            Boot, donde me entusiasma explorar nuevas posibilidades y desafíos.`, `Mi experiencia no se trata solo de aplicar tecnologías actuales sino también de
+            rápidamente
+            adaptarse a nuevas herramientas y entornos de desarrollo. Creo firmemente en combinar la creatividad con la lógica para
+            crear productos de software que no sólo sean visualmente atractivos sino también eficientes y robustos en su
+            funcionalidad.`]}};
+
 const certifications = [
   {
     name: "Aprende a programar con Python",
@@ -49,80 +67,101 @@ const certifications = [
 const projects = [
   {
     title:"Digi-Form",
-    description:`Form built to improve the Agents' productivity. Transform simple clicks in a property's note ready to be sent.`,
+    description:{
+      en: `Form built to improve the Agents' productivity. Transform simple clicks in a property's note ready to be sent.`,
+      es: `Formulario construido para mejorar la productividad de los Agentes. Transforme simples clics en una nota de propiedad lista para ser enviada.`
+    },
     image:"assets/images/digi-form.png",
     link:"https://digi-form.vercel.app/"
   },
 
   {
     title:"Monstercat",
-    description:`A small practice to know how to play audio and video from a server using ReactJs based on Monstercat.`,
-    image:"assets/images/monstercat-practice.png",
+    description:{ en: `A small practice to know how to play audio and video from a server using ReactJs based on Monstercat.`,
+                  es: `Una pequeña práctica para saber reproducir audio y vídeo desde un servidor usando ReactJs basados ​​en Monstercat.`
+    },
+  image:"assets/images/monstercat-practice.png",
     link:"https://monstercat-practice.vercel.app/"
   },
 
   {
     title:"Hulu clone",
-    description:"Updated list of series and trending movies.",
-    image:"assets/images/hulu-clone.png",
+    description:{ en: "Updated list of series and trending movies.",
+                  es: `Lista actualizada de series y películas de tendencia.`
+    },
+  image:"assets/images/hulu-clone.png",
     link:"https://hulu-clone-pearl-alpha.vercel.app/"
   },
 
   {
     title:"Rick & Morty",
-    description:"Website based on rick and morty tv show.",
-    image:"assets/images/rick-and-morty.png",
+    description:{ en: "Website based on rick and morty tv show.",
+                  es: `Sitio basado en la serie animada Rick y Morty`
+    },
+  image:"assets/images/rick-and-morty.png",
     link:"https://react-rick-and-morty-gamma.vercel.app/"
   },
   {
     title:"Music Player",
-    description:"A music player built in Java using Swing and Maven.",
-    image:"assets/images/music_player.png",
+    description:{ en: "A music player built in Java using Swing and Maven.",
+                  es: `Sitio web basado en el programa de televisión rick y morty.`
+    },
+  image:"assets/images/music_player.png",
     link:"https://github.com/Annimus1/MusicPlayer"
   },
   {
     title:"Weather App",
-    description:"Provides interesting data about the current weather of a specific city.",
-    image:"assets/images/weather-app.png",
+    description:{ en: "Provides interesting data about the current weather of a specific city.",
+                  es: `Proporciona datos interesantes sobre el clima actual de una ciudad específica.`
+    },
+  image:"assets/images/weather-app.png",
     link:"https://weather-app-blue-three.vercel.app/"
   },
   {
     title:"Car Workshop",
-    description:"Mockup Carshop service website with a call to action button and background slides animation.",
-    image:"assets/images/car-landingpage.png",
+    description:{ en: "Mockup Carshop service website with a call to action button and background slides animation.",
+                  es: `Maqueta del sitio web del servicio Carshop con un botón de llamada a la acción y animación de diapositivas de fondo.`
+    },
+  image:"assets/images/car-landingpage.png",
     link:"https://annimus1.github.io/car-workshop/"
   },
   {
     title:"Abstract Help Page",
-    description:"Recreation of the Abstract's Help page as chalenge from frontendpractice.",
-    image:"assets/images/Abstract.png",
+    description:{ en: "Recreation of the Abstract's Help page as challenge from frontend practice.",
+                  es: `Recreación de la página de ayuda del resumen como desafío desde la práctica frontend.`
+    },
+  image:"assets/images/Abstract.png",
     link:"https://abstract-help-page-henna.vercel.app/"
   },
   {
     title:"QA Calculator",
-    description:"This app helps to QA_Rating's members to qualify leads.",
-    image:"assets/images/QA.png",
+    description:{ en: "This app helps to QA_Rating's members to qualify leads.",
+                  es: `Esta aplicación ayuda a los miembros de QA_Rating a calificar clientes potenciales.`
+    },
+  image:"assets/images/QA.png",
     link:"https://github.com/Annimus1/QA_CALCULATOR"
   },
   {
     title:"Testimonial page",
-    description:"A clone of testimonial page from FreeCodeCamprops.",
-    image:"assets/images/freeCodeCamp-testimony.png",
+    description:{ en: "A clone of testimonial page from FreeCodeCamp props.",
+                  es: `Un clon de la página de testimonios de los accesorios de FreeCodeCamp.`
+    },
+  image:"assets/images/freeCodeCamp-testimony.png",
     link:"https://freecodecamp-foro.vercel.app/"
   },
 ]
 
 
 // functions 
-function projectTemplate(props){
+function projectTemplate(props, language){
   // convert object to html card representing a single project
   return `
   <div class="col mb-3">
-    <div class="card shadow p-3 bg-body rounded mx-auto" style="width: 18rem; height: 24rem">
+    <div class="card shadow p-3 bg-body rounded mx-auto" style="width: 18rem; height: 26rem">
       <img src=${props.image} class="card-img-top projectImage" alt=${props.title}>
       <div class="card-body">
         <h5 class="card-title align-center">${props.title}</h5>
-        <p class="card-text" style="height: 7rem">${props.description}</p>
+        <p class="card-text" style="height: 9rem">${props.description[language]}</p>
         <div class="d-grid gap-2 col-6 mx-auto">
           <a 
           target="blank"
@@ -155,18 +194,70 @@ function certificationTemplate(props){
 }
 
 
-// filling up the content with all projects
-projects.forEach( project => {
-  projectsContent += projectTemplate(project);
-}) 
+function introductionTemplate(props, language){
+  var contentAux = "";
+  contentAux += `<h1 class="text-center card-title">${props[language].title}</h1>`;
 
-// filling up the content with certifications
-certifications.forEach( certification => {
-  certificationsContent += certificationTemplate(certification)
-})
+  props[language].content.forEach( paragraph => {
+
+    contentAux += `<p class="p">${paragraph}</p>`;
+  })
+
+  return contentAux;
+}
+
+function getActualLenguage(){
+  return languageBtn.checked ? "es":"en";
+}
+
+function colorLanguagesLabel(){
+  const en = document.getElementById("en");
+  const es = document.getElementById("es");
+
+  // delete class primary
+  en.classList.remove("text-primary")
+  es.classList.remove("text-primary")
+  en.classList.remove("text-light")
+  es.classList.remove("text-light")
+  
+  // add clas just to
+  if(!languageBtn.checked){
+    
+    en.classList.add("text-primary")
+    es.classList.add("text-light")
+  }else{
+    en.classList.add("text-light")
+    es.classList.add("text-primary")
+  }
+  
+}
+
+function main(){
+  // variables to save the content
+  let projectsContent = ""
+  let certificationsContent = ""
+
+  // color active language
+  colorLanguagesLabel();
+
+  // filling up the content with all projects
+  projects.forEach( project => {
+    projectsContent += projectTemplate(project, getActualLenguage());
+  }) 
+
+  // filling up the content with certifications
+  certifications.forEach( certification => {
+    certificationsContent += certificationTemplate(certification)
+  })
+
+  // adding info to the div container.
+  introductionContainer.innerHTML = introductionTemplate(introduction, getActualLenguage());
+  projectsContainer.innerHTML = projectsContent;
+  certificationContainer.innerHTML = certificationsContent;
+}
 
 
-
-// adding info to the div container.
-projectsContainer.innerHTML = projectsContent
-certificationContainer.innerHTML = certificationsContent
+document.addEventListener("DOMContentLoaded", main);
+languageBtn.addEventListener("click", ()=>{
+  main();
+});
